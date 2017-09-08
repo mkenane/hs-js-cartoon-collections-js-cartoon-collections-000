4aa4e1e3d994dc8fb2850b2174ebee36a7121a92
Mor Kenane <morkenan@hotmail.com>
2017-09-08 17:24:04 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 function dwarfRollCall(dwarves) {
+  var rollcall = []
+for (var i = 0; i < dwarves.length; i++) {
+rollcall.push((i+1) + ". " + dwarves[i])
 }
+  return rollcall.join(" ") + " "
+}
+
 
 function summonCaptainPlanet(planeteerCalls){
+var screamingcaptain = []
+for (var i = 0; i < planeteerCalls.length; i++) {
+  screamingcaptain.push(planeteerCalls[i].toUpperCase + "!")
 }
+return screamingcaptain
+}
+
 
 function longPlaneteerCalls(words) {
+var filteredarray = words.filter(function(word) {
+  return word.length > 4})
+if (filteredarray.length > 0) {return true}
+else {return false}
 }
 
-function findTheCheese (foods) {
-}
+function findTheCheese(foods) {
+var filteredfoods = foods.filter(function(food) {
+return (food === "cheddar" || food === "gouda" || food === "camembert")})
+if (filteredfoods.length > 0) {return filteredfoods[0]}
+else {return "no cheese!"}
+    }
